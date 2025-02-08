4312c70d0f2a501d6889c9987d6a298810a6466b
AngelTools <drakegotraped1234@gmail.com>
2025-02-09 00:14:28 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+We cant actually host the file on here because github decided to take it down instantly,
+
+Please head over to: https://angeltools.cc
+
+or, alternatively, check out: discord.gg/opensource!
